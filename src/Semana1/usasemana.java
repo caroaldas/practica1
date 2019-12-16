a9acf9cdcbcc6a2792e2db9c2f4e682d3562f36e
user <user@DESKTOP-9KDS4U9>
2019-12-16 07:55:54 -0500

    ES LA PRIMERA PARTE DEL PROYECTO

---
+++ src/Semana1/usasemana.java
@@ -0,0 +1,13 @@
+
+package Semana1;
+
+
+public class usasemana {
+    
+    public static void main(String[] args) {
+        semana s=new semana();
+        s.setVisible(true);
+        s.setLocationRelativeTo(null);
+    }
+    
+}
